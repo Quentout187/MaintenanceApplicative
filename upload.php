b8ca50a716cd8243dd369b00e2395e14a90a6658
Quentout187 <quentin.caron@ecoles-epsi.net>
2024-03-06 11:45:14 +0100

    add code

--- upload.php
+++ upload.php
@@ -2,18 +2,10 @@
     if(isset($_POST['envoyer'])){
         $dossier = 'upload/';
         $fichier = basename($_FILES['fichier']['name']);
-        $typeFichier = $_FILES['fichier']['type'];
-        
-        // Vérifier si le fichier est une image
-        $extensionsAutorisees = array('image/jpeg', 'image/png', 'image/gif');
-        if(in_array($typeFichier, $extensionsAutorisees)){
-            if(move_uploaded_file($_FILES['fichier']['tmp_name'], $dossier . $fichier)){
-                echo 'Upload effectué avec succès !';
-            }else{
-                echo 'Echec de l\'upload !';
-            }
+        if(move_uploaded_file($_FILES['fichier']['tmp_name'], $dossier . $fichier)){
+            echo 'Upload effectué avec succès !';
         }else{
-            echo 'Seuls les fichiers images sont autorisés !';
+            echo 'Echec de l\'upload !';
         }
     }
 ?>
