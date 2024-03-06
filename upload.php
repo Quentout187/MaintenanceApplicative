23939362d9c18d3861ec28f901d41106a38ae1e5
Quentout187 <quentin.caron@ecoles-epsi.net>
2024-03-06 12:30:03 +0100

    add

--- upload.php
+++ upload.php
@@ -1,11 +1,11 @@
 <?php
-    if(isset($_POST['envoyer'])){
-        $dossier = 'upload/';
-        $fichier = basename($_FILES['fichier']['name']);
-        if(move_uploaded_file($_FILES['fichier']['tmp_name'], $dossier . $fichier)){
-            echo 'Upload effectué avec succès !';
-        }else{
-            echo 'Echec de l\'upload !';
-        }
+if(isset($_POST['envoyer'])){
+    $dossier = 'upload/';
+    $fichier = basename($_FILES['fichier']['name']);
+    if(move_uploaded_file($_FILES['fichier']['tmp_name'], $dossier . $fichier)){
+        echo 'Upload effectué avec succès !';
+    }else{
+        echo 'Echec de l\'upload !';
     }
+}
 ?>
