706bdc9918efdd55cf9ef93169a57ae72ec71899
Quentout187 <quentin.caron@ecoles-epsi.net>
2024-03-07 11:28:49 +0100

    ajout de code

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
 <body class="main_page">
 <div class="center_page">
     <?php include 'header.html';?>
+    <img src="upload/Quentin.png" class="logo">
+        <h2>Quentin Caron</h2>
+        <h2>Groupe : Yanis et Vladimir</h2>
+        <br>
     <bouton class="bouton_link">
         <a class="link" href="upload.php">
             Upload your file
